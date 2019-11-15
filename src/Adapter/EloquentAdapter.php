1ccbe6568f8209cdb249d6f166b29c4309a07a8e
Toby Zerner <toby.zerner@gmail.com>
2019-11-16 10:14:12 +1030

    Remove old code

--- src/Adapter/EloquentAdapter.php
+++ src/Adapter/EloquentAdapter.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use InvalidArgumentException;
 use Tobyz\JsonApiServer\Schema\Attribute;
 use Tobyz\JsonApiServer\Schema\HasMany;
@@ -191,8 +190,6 @@ class EloquentAdapter implements AdapterInterface
 
     public function load(array $models, array $relationships, Closure $scope): void
     {
-        $relationship = end($relationships);
-
         (new Collection($models))->loadMissing([
             $this->getRelationshipPath($relationships) => $scope
         ]);
