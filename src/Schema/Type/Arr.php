e1c6901ca4648705f2d0393ab0ada3ecb0121cfc
Bert Ramakers <bertramakers@me.com>
2023-11-10 09:35:52 +0100

    Add array type

---
+++ src/Schema/Type/Arr.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tobyz\JsonApiServer\Schema\Type;
+
+class Arr implements TypeInterface
+{
+    private int $minItems = 0;
+    private ?int $maxItems = null;
+    private bool $uniqueItems = false;
+    private ?TypeInterface $items = null;
+
+    public static function make(): static
+    {
+        return new static();
+    }
+
+    public function serialize(mixed $value): mixed
+    {
+        return $value;
+    }
+
+    public function deserialize(mixed $value): mixed
+    {
+        return $value;
+    }
+
+    public function validate(mixed $value, callable $fail): void
+    {
+        if (!is_array($value)) {
+            $fail('must be an array');
+            return;
+        }
+
+        if (count($value) < $this->minItems) {
+            $fail(sprintf('must contain at least %d values', $this->minItems));
+        }
+
+        if ($this->maxItems !== null && count($value) > $this->maxItems) {
+            $fail(sprintf('must contain no more than %d values', $this->maxItems));
+        }
+
+        if ($this->uniqueItems && count($value) !== count(array_unique($value))) {
+            $fail('must contain unique values');
+        }
+
+        if ($this->items) {
+            foreach ($value as $item) {
+                $this->items->validate($item, $fail);
+            }
+        }
+    }
+
+    public function schema(): array
+    {
+        return [
+            'type' => 'array',
+            'minItems' => $this->minItems,
+            'maxItems' => $this->maxItems,
+            'uniqueItems' => $this->uniqueItems,
+            'items' => $this->items?->schema(),
+        ];
+    }
+
+    public function minItems(int $minItems): static
+    {
+        $this->minItems = $minItems;
+
+        return $this;
+    }
+
+    public function maxItems(?int $maxItems): static
+    {
+        $this->maxItems = $maxItems;
+
+        return $this;
+    }
+
+    public function uniqueItems(bool $uniqueItems = true): static
+    {
+        $this->uniqueItems = $uniqueItems;
+
+        return $this;
+    }
+
+    public function items(?TypeInterface $type): static
+    {
+        $this->items = $type;
+
+        return $this;
+    }
+}
---
+++ tests/feature/ArrTest.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace Tobyz\Tests\JsonApiServer\feature;
+
+use Tobyz\JsonApiServer\Endpoint\Create;
+use Tobyz\JsonApiServer\Exception\UnprocessableEntityException;
+use Tobyz\JsonApiServer\JsonApi;
+use Tobyz\JsonApiServer\Schema\Field\Attribute;
+use Tobyz\JsonApiServer\Schema\Type\Arr;
+use Tobyz\JsonApiServer\Schema\Type\Str;
+use Tobyz\Tests\JsonApiServer\AbstractTestCase;
+use Tobyz\Tests\JsonApiServer\MockResource;
+
+class ArrTest extends AbstractTestCase
+{
+    private JsonApi $api;
+
+    public function setUp(): void
+    {
+        $this->api = new JsonApi();
+    }
+
+    public function test_validates_array()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make())
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $this->expectException(UnprocessableEntityException::class);
+
+        $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => ['type' => 'customers', 'attributes' => ['featureToggles' => 1]],
+            ]),
+        );
+    }
+
+    public function test_invalid_min_length()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make()->minItems(1))
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $this->expectException(UnprocessableEntityException::class);
+
+        $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => ['type' => 'customers', 'attributes' => ['featureToggles' => []]],
+            ]),
+        );
+    }
+
+    public function test_invalid_max_length()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make()->maxItems(1))
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $this->expectException(UnprocessableEntityException::class);
+
+        $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => ['type' => 'customers', 'attributes' => ['featureToggles' => [1, 2]]],
+            ]),
+        );
+    }
+
+    public function test_invalid_uniqueness()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make()->uniqueItems())
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $this->expectException(UnprocessableEntityException::class);
+
+        $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => ['type' => 'customers', 'attributes' => ['featureToggles' => [1, 1]]],
+            ]),
+        );
+    }
+
+    public function test_valid_items_constraints()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(
+                            Arr::make()
+                                ->minItems(2)
+                                ->maxItems(4)
+                                ->uniqueItems(),
+                        )
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $response = $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => ['type' => 'customers', 'attributes' => ['featureToggles' => [1, 2, 3]]],
+            ]),
+        );
+
+        $this->assertJsonApiDocumentSubset(
+            ['data' => ['attributes' => ['featureToggles' => [1, 2, 3]]]],
+            $response->getBody(),
+            true,
+        );
+    }
+
+    public function test_invalid_items()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make()->items(Str::make()->enum(['valid'])))
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $this->expectException(UnprocessableEntityException::class);
+
+        $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => [
+                    'type' => 'customers',
+                    'attributes' => ['featureToggles' => ['valid', 'invalid']],
+                ],
+            ]),
+        );
+    }
+
+    public function test_valid_items()
+    {
+        $this->api->resource(
+            new MockResource(
+                'customers',
+                endpoints: [Create::make()],
+                fields: [
+                    Attribute::make('featureToggles')
+                        ->type(Arr::make()->items(Str::make()->enum(['valid1', 'valid2'])))
+                        ->writable(),
+                ],
+            ),
+        );
+
+        $response = $this->api->handle(
+            $this->buildRequest('POST', '/customers')->withParsedBody([
+                'data' => [
+                    'type' => 'customers',
+                    'attributes' => ['featureToggles' => ['valid1', 'valid2']],
+                ],
+            ]),
+        );
+
+        $this->assertJsonApiDocumentSubset(
+            ['data' => ['attributes' => ['featureToggles' => ['valid1', 'valid2']]]],
+            $response->getBody(),
+            true,
+        );
+    }
+
+    public function test_schema()
+    {
+        $this->assertEquals(
+            [
+                'type' => 'array',
+                'minItems' => 0,
+                'maxItems' => null,
+                'uniqueItems' => false,
+                'items' => null,
+            ],
+            Arr::make()->schema(),
+        );
+
+        $this->assertEquals(
+            [
+                'type' => 'array',
+                'minItems' => 1,
+                'maxItems' => 10,
+                'uniqueItems' => true,
+                'items' => [
+                    'type' => 'string',
+                    'enum' => ['valid1', 'valid2'],
+                    'minLength' => 0,
+                    'maxLength' => null,
+                    'pattern' => null,
+                    'format' => null,
+                ],
+            ],
+            Arr::make()
+                ->minItems(1)
+                ->maxItems(10)
+                ->uniqueItems()
+                ->items(Str::make()->enum(['valid1', 'valid2']))
+                ->schema(),
+        );
+    }
+}
