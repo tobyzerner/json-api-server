7f108dd5af9b7865147e1f09b574ed0aed361be1
Sami Mazouz <sychocouldy@gmail.com>
2024-03-29 11:21:00 +0100

    feat: allow including relationships by default (#96)
    * feat: default include
    
    * Run Prettier
    
    * chore: unnecessary import
    
    ---------
    
    Co-authored-by: SychO9 <SychO9@users.noreply.github.com>

--- src/Endpoint/Concerns/IncludesData.php
+++ src/Endpoint/Concerns/IncludesData.php
@@ -8,9 +8,20 @@ use Tobyz\JsonApiServer\Schema\Field\Relationship;
 
 trait IncludesData
 {
+    protected ?array $defaultInclude = null;
+
+    public function defaultInclude(array $include): static
+    {
+        $this->defaultInclude = $include;
+
+        return $this;
+    }
+
     private function getInclude(Context $context): array
     {
-        if ($includeString = $context->request->getQueryParams()['include'] ?? null) {
+        if (
+            $includeString = $context->request->getQueryParams()['include'] ?? $this->defaultInclude
+        ) {
             $include = $this->parseInclude($includeString);
 
             $this->validateInclude(
